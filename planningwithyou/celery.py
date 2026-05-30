9d17ad4b20354bd3a758e8487a9e0c4be43eecd0
Rafael Pascual <rpascual0812@gmail.com>
2026-05-30 11:39:17 +0800

    updated celery

--- planningwithyou/celery.py
+++ planningwithyou/celery.py
@@ -6,8 +6,8 @@ from celery import Celery
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'planningwithyou.settings')
 
 app = Celery('planningwithyou')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
+app.conf.broker_url = os.environ.get('CELERY_BROKER_URL', 'redis://localhost:6379/0')
+app.conf.result_backend = os.environ.get('CELERY_RESULT_BACKEND', 'redis://localhost:6379/0')
 
 app.conf.broker_transport_options = {
     "ssl_cert_reqs": ssl.CERT_NONE
@@ -16,3 +16,6 @@ app.conf.broker_transport_options = {
 app.conf.result_backend_transport_options = {
     "ssl_cert_reqs": ssl.CERT_NONE
 }
+
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
