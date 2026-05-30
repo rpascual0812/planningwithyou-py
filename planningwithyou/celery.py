fadf75125f670b1f4ac6f458de3449701ea21aa6
Rafael Pascual <rpascual0812@gmail.com>
2026-05-30 11:36:14 +0800

    Added ssl_cert_reqs to celery.py

--- planningwithyou/celery.py
+++ planningwithyou/celery.py
@@ -1,4 +1,5 @@
 import os
+import ssl
 
 from celery import Celery
 
@@ -7,3 +8,11 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'planningwithyou.settings')
 app = Celery('planningwithyou')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
+
+app.conf.broker_transport_options = {
+    "ssl_cert_reqs": ssl.CERT_NONE
+}
+
+app.conf.result_backend_transport_options = {
+    "ssl_cert_reqs": ssl.CERT_NONE
+}
